5d148332d9be98553081344dd35b0b1e0a797e30
unknown <harishimp0077@gmail.com>
2021-04-28 18:58:01 +0530

    Added header component

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAPCKJ6W1QFPIayh6mn59Jbh-lGLs032lM",
+    authDomain: "e-commerce-app-21468.firebaseapp.com",
+    projectId: "e-commerce-app-21468",
+    storageBucket: "e-commerce-app-21468.appspot.com",
+    messagingSenderId: "23000322393",
+    appId: "1:23000322393:web:723e1f850a8717cf713c48",
+    measurementId: "G-KCFMKDMF34"
+  };
